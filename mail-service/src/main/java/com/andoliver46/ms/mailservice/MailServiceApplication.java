36f9b9581b1a486fe4b7ce86350ca8a68c9d439a
AndOliver46 <anderson_souza75@live.com>
2024-02-14 18:24:55 -0300

    feat: mail-ms service created, postgres database and service port configured

---
+++ mail-service/src/main/java/com/andoliver46/ms/mailservice/MailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.andoliver46.ms.mailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailServiceApplication.class, args);
+	}
+
+}
---
+++ mail-service/src/test/java/com/andoliver46/ms/mailservice/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliver46.ms.mailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

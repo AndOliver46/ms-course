4edabe1d30fde845b1d00b1d6edb82ff90c5a0c2
AndOliver46 <anderson_souza75@live.com>
2024-02-14 18:12:56 -0300

    feat: user-ms service created, postgres database and service port configured

---
+++ user-service/src/test/java/com/andoliver46/ms/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliver46.ms.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

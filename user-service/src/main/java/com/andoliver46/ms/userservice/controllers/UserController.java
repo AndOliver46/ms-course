68fe1b6ea8bcd7606ff09dce4a951a9d593d931a
AndOliver46 <anderson_souza75@live.com>
2024-02-14 21:15:37 -0300

    feat: userservice injected in controller, saving to the database (tested)

--- user-service/src/main/java/com/andoliver46/ms/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/andoliver46/ms/userservice/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.andoliver46.ms.userservice.controllers;
 
 import com.andoliver46.ms.userservice.dtos.UserRecordDTO;
 import com.andoliver46.ms.userservice.models.UserModel;
+import com.andoliver46.ms.userservice.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
@@ -13,10 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
     @PostMapping("/users")
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDTO userRecordDTO){
         var userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDTO,userModel);
+        userService.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
 }

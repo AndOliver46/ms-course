69d533419d349f26608c57d7bbc12853a175655f
AndOliver46 <anderson_souza75@live.com>
2024-02-15 11:22:44 -0300

    feat: injected mailservice in mailconsumer and calling sendEmail method

--- mail-service/src/main/java/com/andoliver46/ms/mailservice/consumers/MailConsumer.java
+++ mail-service/src/main/java/com/andoliver46/ms/mailservice/consumers/MailConsumer.java
@@ -2,6 +2,7 @@ package com.andoliver46.ms.mailservice.consumers;
 
 import com.andoliver46.ms.mailservice.dtos.EmailRecordDTO;
 import com.andoliver46.ms.mailservice.models.EmailModel;
+import com.andoliver46.ms.mailservice.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,11 +11,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class MailConsumer {
 
+    private final EmailService emailService;
+
+    public MailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.ms-mail.name}")
     public void listenEmailQueue(@Payload EmailRecordDTO email){
         var emailModel = new EmailModel();
         BeanUtils.copyProperties(email, emailModel);
-        //send email
+
+        emailService.sendEmail(emailModel);
     }
 
 }
